7a1cdfbc3ccb5d203fae6c057b3299c3e26cdddb
Augusto Renan <augustorenanss@gmail.com>
2022-06-12 16:06:22 -0300

    feat(Swagger): added api documentation

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
 import VehicleRoutes from './routes/VehicleRoutes'
+import swaggerDocument from './swagger.json'
 
 const app = express()
 
 app.use(express.json())
+
+//Docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+//Resources routes
 app.use(VehicleRoutes)
 
 export default app
